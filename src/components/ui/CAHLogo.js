26e481c895914983cc4a21d59c6fdafda05f621a
Diego Bugmann <digi.bugi@hotmail.com>
2022-04-03 23:06:27 +0200

    header icon clickable + some css

--- src/components/ui/CAHLogo.js
+++ src/components/ui/CAHLogo.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "styles/ui/CAHLogo.scss";
 import CAH_logo from '../../CAH_logo3.png';
 
 export const CAHLogo = props => {
   return (
-    <img src={CAH_logo} />
+    <Link to="/lobby"><a><img src={CAH_logo}/></a></Link>
   );
 };
