8cdb22642a2f59c8d050675aaba2bba43e7cde15
Szymon Kaczmarski <szymon.kaczmarski@uzh.ch>
2022-04-11 12:40:53 +0200

    added displaying the name of the game, minor styling issues fixed

--- src/components/views/Lobby.js
+++ src/components/views/Lobby.js
@@ -1,7 +1,5 @@
 import {useState} from 'react';
 import {api,catchError, updateApi, handleError} from 'helpers/api';
-import {Spinner} from 'components/ui/Spinner';
-import {SpinnerSquares} from 'components/ui/SpinnerSquares';
 import {SpinnerBalls} from 'components/ui/SpinnerBalls';
 import {Button} from 'components/ui/Button';
 import {useHistory} from 'react-router-dom';
@@ -10,7 +8,6 @@ import PropTypes from "prop-types";
 import {useInterval} from 'helpers/utils';
 import "styles/views/Lobby.scss";
 import SideBar from "components/views/SideBar";
-
 
 
 // -------------------------------- LOBBY --------------------------------
@@ -34,8 +31,8 @@ const Lobby = () => {
   const Game = ({game}) => (
       <div className="game container"
            onClick={() => joinGame(game.gameId)}>    
-        <div className="game id">{game.gameId}</div>
         <div className="game name">{game.gameName}</div>
+        <div className="game numberOfRounds">{game.numOfRounds}</div>
         <div className="game numberOfPlayers">{game.numOfPlayersJoined} / 4</div>
         <div className="game cardsType">{game.gameEdition}</div>
         <div className="game gameMode">{game.cardCzarMode ? "Card Czar" : "Points"}</div>
@@ -62,23 +59,6 @@ const Lobby = () => {
     }
     fetchData();
   }, 1000);
-
-  // -------------------------------- LOGOUT --------------------------------
-  // const logout = async () => {
-  //   try {
-  //     // prepare logout API call
-  //     await api.post('/users/logout');
-  //     localStorage.removeItem('token');
-  //     localStorage.removeItem('loggedInUserID');
-  //     // and update the API, to NOT include Authorization for future requests
-  //     updateApi();
-
-  //     history.push('/login');
-
-  //   } catch (error) {
-  //     alert(`Something went wrong during the logout: \n${handleError(error)}`);
-  //   }
-  // }
   // -------------------------------- SPINNER --------------------------------
   let content = <SpinnerBalls/>;
   // -------------------------------- IF --------------------------------
@@ -90,8 +70,8 @@ const Lobby = () => {
           <h2>Games</h2>
           <h5>Join by clicking on the game!</h5>
           <div className="tableHeader container">
-            <div>ID</div>
             <div>Name</div>
+            <div>Number of rounds</div>
             <div>Players</div>
             <div>Cards</div>
             <div>Game Mode</div>
@@ -110,14 +90,6 @@ const Lobby = () => {
             >
               Create a new game
             </Button>
-
-            {/* LOGOUT BUTTON 
-            <Button
-              width="100%"
-              onClick={() => logout()}
-            >
-              Logout
-            </Button> */}
           </div>
         </div>
       </div>
