d407f6b39e509eeb11f02e6ca9048bde92a9c92f
Diego Bugmann <digi.bugi@hotmail.com>
2022-04-14 07:37:01 +0200

    updates regarding polling / useEffect (not final)

--- src/components/views/GameView.js
+++ src/components/views/GameView.js
@@ -29,21 +29,29 @@ const GameView = () => {
 
   const { gameId } = useParams();
   const history = useHistory();
+
   // TODO maybe role & whiteCards can be combined in a Player
   // const [role, setRole] = useState(null);
   // const [whiteCards, setWhiteCards] = useState(null);
   const [player, setPlayer] = useState(null);
+
   // TODO maybe blackCard, roundNum & choices can be combined in a Round
-  // const [blackCard, setBlackCard] = useState(null);
+  const [blackCard, setBlackCard] = useState(null);
   const [roundNr, setRoundNr] = useState(1);
-  // const [choices, setChoices] = useState(null);
-  const [round, setRound] = useState(null);
+  const [choices, setChoices] = useState(null);
+  // const [round, setRound] = useState(null);
+
+  // TODO update this counter whenever a player successfully played a card
+  const [cardsPlayed, setCardsPlayed] = useState(0);
+
   // const [scores, setScores] = useState(null);
+
   const [winner, setWinner] = useState(null);
 
   /*
    useEffect is used to always fetch new Player-data (white cards & role) 
-   whenever a new round starts (so when roundNr changes!)
+   whenever a new round starts (so when roundNr changes!) OR when
+   a card was played (to only display 9 white cards)
   */
   useEffect(async () => {
     try {
@@ -54,11 +62,19 @@ const GameView = () => {
     } catch (error) {
       catchError(history, error, 'fetching the player data');
     }
-  }, [roundNr]);
+  }, [roundNr, cardsPlayed]);
+
 
   // it might also be used to show the winner when this state changes
   useEffect(() => {
-    // TODO
+    /*
+    TODO add a 10 second countdown until the round and blackCard are set and therefore updated
+    This is for the players to actually see the winner!
+    */
+
+
+    // TODO setRoundNr & setBlackCard
+    // setRoundNr(response.data.roundNr);
   }, [winner]);
 
 
@@ -73,33 +89,36 @@ const GameView = () => {
     // if new round data is available, display the new data
     fetchRoundData();
 
-    // if a winner was decided, display the winner!
-    fetchWinner();
+    // if a winner was decided, display the winner! (INCLUDED IN THE ROUND!)
+    // fetchWinner();
 
   }, 5000);
 
   const fetchRoundData = async () => {
     try {
+      // console.log("FETCH ROUND DATA");
       // const response = await api.get(`/${gameId}/gameround`);
       setRoundNr(roundNr + 1); //TODO delete
-      // setRoundNr(response.data.roundNr);
-      // TODO setBlackCard and setChoices (played Cards) OR just setRound
-      // console.log("FETCH ROUND DATA");
+
+      // TODO setChoices (played Cards) - always update the choices when they change
+
+      // TODO setWinner - always update the winner (will trigger useEffect when actually changing)
+      
     } catch (error) {
       catchError(history, error, 'fetching the round data');
     }
   }
 
-  const fetchWinner = async () => {
-    try {
-      // TODO
-      // const response = await api.get(`/${gameId}/roundwinner`);
-      // setWinner() ...
-      // console.log("FETCH WINNER DATA");
-    } catch (error) {
-      catchError(history, error, 'fetching the winner');
-    }
-  }
+  // const fetchWinner = async () => {
+  //   try {
+  //     // TODO
+  //     // const response = await api.get(`/${gameId}/roundwinner`);
+  //     // setWinner() ...
+  //     // console.log("FETCH WINNER DATA");
+  //   } catch (error) {
+  //     catchError(history, error, 'fetching the winner');
+  //   }
+  // }
 
 
   return (
