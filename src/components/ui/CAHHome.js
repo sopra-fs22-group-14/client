ab21564cee398bc0f3385397b2abe2f29f6ccc82
Diego Bugmann <digi.bugi@hotmail.com>
2022-04-24 17:12:57 -0500

    started "HOME"-design

---
+++ src/components/ui/CAHHome.js
@@ -0,0 +1,9 @@
+import React from "react";
+import CAH_Home from '../../CAH_Home.jpeg';
+import 'styles/ui/CAHHome.scss';
+
+export const CAHHome = props => {
+  return (
+    <img src={CAH_Home} className="cahHome"/>
+  );
+};
--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,9 +1,41 @@
+import React from "react";
+import {CAHHome} from "components/ui/CAHHome";
+import BaseContainer from "components/ui/BaseContainer";
+import {Button} from 'components/ui/Button';
+import 'styles/views/Home.scss';
+import {useHistory} from 'react-router-dom';
+import {handleError} from 'helpers/api';
 
 
 const Home = props => {
-    return (
-        <h2>Home</h2>
-    );
+
+  const history = useHistory();
+
+  const goPlay = () => {
+    try {
+      history.push('/lobby')
+    } catch (error) {
+      alert(`Something went wrong during the redirection: \n${handleError(error)}`);
+    }
+  }
+  
+  return (
+    <BaseContainer>
+      <h2>
+        CARDS AGAINST HUMANITY - fun for everyone!
+      </h2>
+      <div className="home">
+        <div className="home paragraph">
+          "Stupid"<br/><br/>-Bloomberg
+        </div>
+        <CAHHome/>
+        <div className="home paragraph">
+          "Hysterical"<br/><br/>-TIME
+        </div>
+      </div>
+      <Button width="50%" onClick={() => goPlay()}>Play Now</Button>
+    </BaseContainer>
+  );
 }
 
 export default Home;
